1c524a2d2d62354e1c120720173786cb605be408
Nixon Davila <davilanixon8@gmail.com>
2024-10-11 13:58:14 -0600

    Cambios del controlador

--- src/controllers/productsControllers.js
+++ src/controllers/productsControllers.js
@@ -88,4 +88,32 @@ const editar = async (req , res) => {
   }
 }
 
-module.exports = { getProducts, getProductsId, eliminar, editar};
+const addProduct = async (req, res) => {
+    const {name, precio, cantidad, fecha_de_vencimiento} = req.body
+    //validacion para que sea obligatorio el guardado de el post
+    if (!name || !precio || !cantidad || !fecha_de_vencimiento) {
+        return res.status(400).json({
+            message: 'Todos los datos son obligatorios: name y precio'
+        })
+    }
+    try {
+      const productos = await leerData();
+  
+        const newProducto = {
+            id: productos.Productos.length + 1,
+            name,
+            cantidad,
+            precio,
+            fecha_de_vencimiento
+        };  
+  
+        productos.Productos.push(newProducto);
+        const jsonData = JSON.stringify(productos, null, 2);
+        await fs.promises.writeFile(filePath, jsonData);
+        res.status(201).json(newProducto);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al crear el producto', error: error.message });
+    }
+  };
+
+module.exports = { getProducts, getProductsId, addProduct, eliminar, editar };
